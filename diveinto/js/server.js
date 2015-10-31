94a2ad6503a5869383f2556ec69138030fce3faa
MrLokans <trikster1911@gmail.com>
2015-10-31 20:29:18 +0300

    Server set up.

--- diveinto/js/server.js
+++ diveinto/js/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 var bookId = 3;
 
 
@@ -24,9 +25,7 @@ function removeBook(id){
     books.splice(bookIndex, 1);
 }
 
-app.configure(function(){
-    app.use(express.bodyParser());
-});
+app.use(bodyParser.json());   
 
 var books = [
     {id: 1, author: 'Ukio Misima', title: 'Golden Temple', year: 1956},
@@ -79,3 +78,36 @@ app.put('/books/:id', function(request, response){
         response.send(book);
     }   
 });
+
+app.delete('/books/:id', function(request, response){
+    response.header('Access-Control-Allow-Origin', '*');
+    var searchId = parseInt(request.params.id, 10);
+    var book = findBook(searchId);
+
+    if(book == null){
+        console.log("Couldn't find book with id " + searchId);
+        response.send(404);
+    } else {
+        console.log('Deleting book with id ' + searchId);
+        removeBook(searchId);
+        response.send(200);
+    }
+    response.send(200);
+});
+
+// Here we allow CORS requests
+var allowCrossDomain = function(request, response, next){
+    response.header('Access-Control-Allow-Origin', "http://localhost");
+    response.header('Access-Control-Allow-Methods', 'OPTIONS, GET,PUT,POST,DELETE');
+    response.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    if ('OPTIONS' == request.method) {
+        response.send(200);
+    } else {
+        next();
+    }
+};
+
+app.use(allowCrossDomain);
+
+app.listen(8080);
